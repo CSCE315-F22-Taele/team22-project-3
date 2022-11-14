61a69f56e0077f1a50ba57b57481a96f89f3085b
Joshua Batac <joshuabatac@client-10-228-178-255.tamulink.tamu.edu>
2022-11-14 13:59:48 -0600

    radiobutton to switch and added a restart button

--- react-pos/src/App.js
+++ react-pos/src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
+import { Button} from 'react-bootstrap';
 
 function App() {
 
   const handleClick = (param) => {
     console.log(param);
+
+
   };
+  
+  function RestartPage() {
+    window.location.reload(false);
+  }
 
   return (
     <div className="App">
       
       <div className="App-header">
-      <p> CAVA GRILL Online Ordering Service </p>
+      <p> Cabo Grill Online Ordering Service </p>
       </div>
 
       <div className="App-body"> 
-
+        
         <div className= "radio"> 
         <br></br>
         <ul>
@@ -24,7 +31,6 @@ function App() {
           &nbsp;&nbsp;&nbsp;
           <input label="Burrito" type="radio" id="14" name="base" value="0" onClick={event => handleClick("Burrito")} />
           </li>
-
           <div className = "title" > Protein </div>
           <li>
           <input label="Chicken ($7.29)" type="radio" id="0" name="protein" value="7.29" onClick={event => handleClick("Chicken")} /> 
@@ -35,62 +41,69 @@ function App() {
           &nbsp;&nbsp;&nbsp;
           <input label="Vegetables ($7.29)" type="radio" id="3" name="protein" value="7.29" onClick={event => handleClick("Vegetables")}/>
           </li>
+          <li>
+          <input label="None" type="radio" name="protein" value="0" onClick={event => handleClick("None")} />
+          </li>
 
           <div className = "title" > Add Ons </div>
           <li>
-          <input label="Guacamole ($2.00)" type="radio" id="4" name="addOnPrem" value="2.00" onClick={event => handleClick("Guacamole")}/> 
+          <input label="Guacamole ($2.00)" type="checkbox" id="4" value="2.00" onClick={event => handleClick("Guacamole")}/> 
           &nbsp;&nbsp;&nbsp;
-          <input label="Queso ($2.00)" type="radio" id="5" name="addOnPrem" value="2.00" onClick={event => handleClick("Queso")}/> 
+          <input label="Queso ($2.00)" type="checkbox" id="5" value="2.00" onClick={event => handleClick("Queso")}/> 
           </li>
 
           <li>
-          <input label="Rice" type="radio" id="15" name="addOn" value="0.00" onClick={event => handleClick("Rice")}/> 
+          <input label="Rice" type="checkbox" id="15" value="0.00" onClick={event => handleClick("Rice")}/> 
           &nbsp;&nbsp;&nbsp;
-          <input label="Cheese" type="radio" id="16" name="addOn" value="0.00" onClick={event => handleClick("Cheese")}/>
+          <input label="Cheese" type="checkbox" id="16" value="0.00" onClick={event => handleClick("Cheese")}/>
           &nbsp;&nbsp;&nbsp;
-          <input label="Ranch" type="radio" id="17" name="addOn" value="0.00" onClick={event => handleClick("Ranch")}/>
+          <input label="Ranch" type="checkbox" id="17" value="0.00" onClick={event => handleClick("Ranch")}/>
           </li>
 
           <li>
-          <input label="Chipotle" type="radio" id="18" name="addOn" value="0.00" onClick={event => handleClick("Chipotle")} /> 
+          <input label="Chipotle" type="checkbox" id="18" name="abc" value="0.00" onClick={event => handleClick("Chipotle")} /> 
           &nbsp;&nbsp;&nbsp;
-          <input label="Salsa" type="radio" id="19" name="addOn" value="0.00" onClick={event => handleClick("Salse")}/>
+          <input label="Salsa" type="checkbox" id="19"  value="0.00" onClick={event => handleClick("Salse")}/>
           &nbsp;&nbsp;&nbsp;
-          <input label="Sour Cream" type="radio" id="20" name="addOn" value="0.00" onClick={event => handleClick("Sour Cream")}/>
+          <input label="Sour Cream" type="checkbox" id="20"  value="0.00" onClick={event => handleClick("Sour Cream")}/>
           &nbsp;&nbsp;&nbsp;
-          <input label="Cream" type="radio" id="21" name="addOn" value="0.00" onClick={event => handleClick("Cream")}/>
+          <input label="Cream" type="checkbox" id="21"  value="0.00" onClick={event => handleClick("Cream")}/>
           </li>
 
           <div className = "title" > Sides </div>
 
           <li>
-          <input label="Chips and Salsa ($2.19)" type="radio" id="6" name="addOn" value="2.19" onClick={event => handleClick("Chips and Salsa")}/> 
+          <input label="Chips and Salsa ($2.19)" type="checkbox" id="6"  value="2.19" onClick={event => handleClick("Chips and Salsa")}/> 
           &nbsp;&nbsp;&nbsp;
-          <input label="Chips and Queso ($3.64)" type="radio" id="7" name="addOn" value="3.64" onClick={event => handleClick("Chips and Queso")}/> 
+          <input label="Chips and Queso ($3.64)" type="checkbox" id="7"  value="3.64" onClick={event => handleClick("Chips and Queso")}/> 
           &nbsp;&nbsp;&nbsp;
-          <input label="Chips and Guac ($3.69)" type="radio" id="8" name="addOn" value="3.69" onClick={event => handleClick("Chips and Guac")}/> 
+          <input label="Chips and Guac ($3.69)" type="checkbox" id="8"  value="3.69" onClick={event => handleClick("Chips and Guac")}/> 
           </li>
 
           <div className = "title" > Drinks & Desserts </div>
           <li> 
-          <input label="Brownie ($1.99)" type="radio" id="9" name="Dessert" value="1.99" onClick={event => handleClick("Brownie")}/> 
+          <input label="Brownie ($1.99)" type="checkbox" id="9" name="Dessert" value="1.99" onClick={event => handleClick("Brownie")}/> 
           &nbsp;&nbsp;&nbsp;
-          <input label="Cookie ($1.99)" type="radio" id="10" name="Dessert" value="1.99" onClick={event => handleClick("Cookie")}/> 
+          <input label="Cookie ($1.99)" type="checkbox" id="10" name="Dessert" value="1.99" onClick={event => handleClick("Cookie")}/> 
           </li>
           <li> 
-          <input label="16 oz Fountain Drink ($2.25)" type="radio" id="11" name="Drink" value="2.25" onClick={event => handleClick("16 oz Fountain Drink")}/> 
+          <input label="16 oz Fountain Drink ($2.25)" type="checkbox" id="11" name="Drink" value="2.25" onClick={event => handleClick("16 oz Fountain Drink")}/> 
           &nbsp;&nbsp;&nbsp;
-          <input label="22 oz Fountain Drink ($2.75)" type="radio" id="12" name="Drink" value="2.75" onClick={event => handleClick("22 oz Fountain Drink")}/> 
+          <input label="22 oz Fountain Drink ($2.75)" type="checkbox" id="12" name="Drink" value="2.75" onClick={event => handleClick("22 oz Fountain Drink")}/> 
           </li>
 
           <div className = "title" > Etc. </div>
-          <input label="Napkins" type="radio" id="22" name="etc" value="0.00" onClick={event => handleClick("Napkins")}/> 
+          <input label="Napkins" type="checkbox" id="22" name="etc" value="0.00" onClick={event => handleClick("Napkins")}/> 
           &nbsp;&nbsp;&nbsp;
-          <input label="Silverware" type="radio" id="23" name="etc" value="0.0" onClick={event => handleClick("Silverware")}/> 
+          <input label="Silverware" type="checkbox" id="23" name="etc" value="0.0" onClick={event => handleClick("Silverware")}/> 
         </ul>
-       
-        </div>
 
+        <Button onClick = {RestartPage}> Restart </Button>
+
+        <br></br>
+
+        </div>
+        
       </div>
 
     </div>
