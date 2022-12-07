22a54ef2435747f61de164f03cea1c3e45806337
Semion Dyadkin <sema.dyadkin@gmail.com>
2022-12-06 22:48:36 -0600

    added google translate reference and oauth to login

--- webapp/app.js
+++ webapp/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { Pool } = require('pg');
 const dotenv = require('dotenv').config();
-
 //create express app
 const app = express();
 const port = 3000;
+const session = require('express-session');
 
 //create pool
 const pool = new Pool ({
@@ -64,6 +64,14 @@ app.get('/online-order', (req,res) => {
     res.render('online-order');
 });
 
+app.get('/login2', (req,res) => {
+    res.render('login2');
+});
+
+app.get('/successfulLogin', (req,res) => {
+    res.render('successfulLogin');
+});
+
 app.get('/server-view', (req,res) => {
     res.render('server-view');
 });
@@ -71,4 +79,51 @@ app.get('/server-view', (req,res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
+function handleCallbackResponse(response){
+  console.log("Token")
+}
 
+const passport = require('passport');
+var userProfile;
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET' 
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+//for hosted website const GOOGLE_CLIENT_ID = '632511847355-7tqapbka72drbect6f8m38ic3m9c509r.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '632511847355-6m02srevbfvp7l7kt7fdekgcp1es90r9.apps.googleusercontent.com';
+//for hosted website const GOOGLE_CLIENT_SECRET = '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';
+const GOOGLE_CLIENT_SECRET = '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';
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    // callbackURL: "https://cabogrillmsc.onrender.com/auth/google/callback"
+    callbackURL: "http://localhost:3000/auth/google/callback"
+
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/successfulLogin') 
+  });
